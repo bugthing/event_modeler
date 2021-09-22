5667505e6bbbf41dd04de19cf306d5bfd1c9431e
Benjamin Martin <ben.martin@smartpension.co.uk>
2021-09-22 22:06:48 +0100

    make bounded drag things have lanes

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ import model from './model'
 import Bound from './bound'
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    const bound = new Bound(document.getElementById('container'))
-    console.log('adding:', bound);
-    bound.add(document.getElementById('item'));
-    bound.add(document.getElementById('item2'));
+
+    const ui_bound = new Bound(document.getElementById('ui_lane'))
+    const cd_bound = new Bound(document.getElementById('commands_and_read_lane'))
+    const ev_bound = new Bound(document.getElementById('events_lane'))
+
+    ui_bound.add(document.getElementById('item1'))
+    cd_bound.add(document.getElementById('item2'))
+    ev_bound.add(document.getElementById('item3'))
+    cd_bound.add(document.getElementById('item4'))
 
 });
-
